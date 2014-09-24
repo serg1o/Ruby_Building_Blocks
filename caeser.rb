8ea8b8bf08fd3d9912be360a690887d366bfd9d4
Sérgio Ribeiro <sergiomsr@gmail.com>
2014-09-24 08:58:25 +0100

    correction

--- caeser.rb
+++ caeser.rb
@@ -1,18 +1,16 @@
 def cesar(text, shift)
-  shift %= "z".ord - "a".ord #make shift a number between 0 and 25
+  number_of_letters = "z".ord - "a".ord + 1
+  shift %= number_of_letters #make shift a number between 0 and 25
+  if (shift < 0)
+    shift += number_of_letters
+  end
   text.split("").each do |letter|
     if letter.match /[A-Za-z]/
       upcase = letter.upcase == letter ? true : false
       letter.upcase!
       new_letter = nil
-      if shift >= 0
-        if (((letter.ord + shift) > "Z".ord) && (letter <= "Z"))
-          new_letter = ("A".ord + shift - ("Z".ord - letter.ord) -1).chr
-        end
-      else #case when shift < 0
-        if (((letter.ord + shift) < "A".ord) && (letter >= "A"))
-          new_letter = ("Z".ord + shift + (letter.ord - "A".ord) +1).chr
-        end
+      if (((letter.ord + shift) > "Z".ord) && (letter <= "Z"))
+        new_letter = ("A".ord + shift - ("Z".ord - letter.ord) -1).chr
       end
       new_letter ||= (letter.ord + shift).chr
       upcase ? print(new_letter) : print(new_letter.downcase)
